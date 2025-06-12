ac22d63f2c170dabc6031a66a5a5b362cb7e359a
gabriel.ritta <gabriel.ritta@defensoria.ba.def.br>
2025-06-12 17:35:55 -0300

    feature: configurando helperconfig para deploys em várias chains

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -4,11 +4,18 @@ pragma solidity ^0.8.20;
 
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
     function run() external returns (FundMe){
+
+        //Antes do broadcast, não implementamos na blockchain e não pagamos gas
+        HelperConfig helperConfig = new HelperConfig(); //Instanciando o contrato HelperConfig
+        (address priceFeed) = helperConfig.activeNetworkConfig(); //Desestruturando o struct NetworkConfig e pegando o priceFeed
+
+        //Depois do broadcast, implementamos na blockchain e pagamos gas
         vm.startBroadcast();
-        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        FundMe fundMe = new FundMe(priceFeed);
         vm.stopBroadcast();
         return fundMe; //retorna o contrato FundMe
     }
--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,6 +2,8 @@
 
 //1. Deploy the mocks quando estivermos na local anvil chain
 //2. Controlar os contratos de diferentes chains
+//SEPOLIA ETH/USD
+//MAINNET ETH/USD
 
 pragma solidity ^0.8.20;
 
@@ -11,17 +13,47 @@ contract HelperConfig is Script {
     //Se estivermos em uma local anvil chain, vamos usar os mocks
     //Se estivermos em uma chain diferente, vamos usar os contratos que já existem
 
+    //Vamos usar o activeNetworkConfig para controlar o contrato que vamos usar
+    NetworkConfig public activeNetworkConfig;
+
+    //Tipando o price feed
     struct NetworkConfig {
         address priceFeed; //ETH/USD price feed address
     }
 
-    function getSepoliaEthConfig() public pure {
+    //Se o chainId for 11155111, vamos usar o sepoliaConfig
+    //Se o chainId for 31337, vamos usar o anvilConfig
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else if (block.chainid == 1) {
+            activeNetworkConfig = getMainnetEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
+
+    //Recebe o endereço do price feed do ETH/USD
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        //price feed address
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+
+        return sepoliaConfig; //Retorna o endereço do price feed do ETH/USD
+    }
+
+    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
         //price feed address
 
     }
 
-    function getAnvilEthConfig() public pure {
-        //price feed address
+    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+             //price feed address
+        NetworkConfig memory ethConfig = NetworkConfig({
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        });
 
+        return ethConfig; //Retorna o endereço do price feed do ETH/USD
     }
 }
