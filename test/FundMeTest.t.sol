9f9c4af587a68323fd44e54e623ddf658a49b8f9
gabrielfst30 <gabrielfst30@gmail.com>
2025-06-20 01:58:17 -0300

    feature: criando address ficticio e utilizando cheatcodes para testes mais aprimorados

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -15,22 +15,24 @@ contract FundMeTest is Test {
     //Variável do tipo FundMe
     FundMe fundMe;
 
+    address USER = makeAddr("user"); //criando endereço ficticio
+
     //Função de configuração que será executada antes de cada teste
     function setUp() external {
         //Instanciando contrato
         //fundMe é uma variável do tipo FundMe que recebe o contrato FundMe.
         DeployFundMe deployFundMe = new DeployFundMe(); //Instanciando o contrato DeployFundMe
         fundMe = deployFundMe.run(); //Atribuindo o contrato FundMe ao contrato fundMe
+        vm.deal(USER, 10e18); //cheatcode para atribuir valor ao address USER criado
     }
 
-    //Função que será executada para testar o contrato
+    //Testando se o valor mínimo é 5
     function testMinimunDollarIsFive() public view {
-        //Testando se o valor mínimo é 5
         assertEq(fundMe.MINIMUM_USD(), 5e18);
         console.log(fundMe.MINIMUM_USD());
     }
 
-    //Função que será executada para testar o contrato
+    //Testando se quem esta executando o contrato é o dono do contrato
     function testOwnerIsMsgSender() public view {
         //Testando se o owner é o address(this) que é o contrato que está sendo testado
         assertEq(fundMe.i_owner(), msg.sender);
@@ -40,24 +42,28 @@ contract FundMeTest is Test {
         console.log(msg.sender);
     }
 
-    //Função que será executada para testar o contrato
+    //Testando o retorno da versão do priceFeed
     function testPriceFeedVersionIsCorrect() public view {
         //Primeiro vamos chamar o valor para ver o que está retornando
         uint256 version = fundMe.getVersion();
         console.log("Price feed version:", version);
-        
+
         //Depois fazemos a verificação
         assertEq(version >= 4, true);
     }
 
-    //Força falha para quando a transação não enviar nenhum eth
+    //Força um teste de falha para quando a transação não enviar nenhum eth
     function testFundFailsWithoutEnoughEth() public {
-        vm.expectRevert(); //Força a proxima transação a falhar
+        vm.expectRevert(); //cheatcode do foundry que força a falha da próxima linha/transação
         fundMe.fund(); //Chamando a funçao fund sem enviar nenhum ether
     }
 
-
-    function testFundUpdatesFundedDataStructure() public{
-        fundMe.fund{value: 10e18}();
+    //Testando se a estrutura de dados atualiza após receber algum eth
+    function testFundUpdatesFundedDataStructure() public {
+        vm.prank(USER); //cheatcode para fazer a próxima transação ser enviada por USER
+        fundMe.fund{value: 10e18}(); //enviando 10 ETH
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER); //Pegando o address que envio o ether
+        //Verificando se o address foi atualizado com o novo valor de ether enviado
+        assertEq(amountFunded, 10e18);
     }
 }
