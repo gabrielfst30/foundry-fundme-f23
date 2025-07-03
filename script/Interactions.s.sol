040636156341c8ea9a89ab8d44f771554b6b43a7
gabriel.ritta <gabriel.ritta@defensoria.ba.def.br>
2025-07-03 17:04:26 -0300

    refactor: refatorando código

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -9,15 +9,9 @@ import {Script, console} from "forge-std/Script.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 import {FundMe} from "../src/FundMe.sol";
 
+//Script para financiar o contrato
 contract FundFundMe is Script {
     uint256 constant SEND_VALUE = 0.01 ether; //Valor constante para financiar o contrato (economiza gas)
-    //Função para financiar o contrato pegando o contrato mais recentemente implantado
-    function fundFundMe(address mostRecentlyDeployedContract) public {
-        vm.startBroadcast();
-        FundMe(payable(mostRecentlyDeployedContract)).fund{value: SEND_VALUE}(); //Financiando o contrato com 0.01 ether
-        console.log("Funded FundMe with 0.01 ETH: ", SEND_VALUE);
-        vm.stopBroadcast();
-    }
 
     //Função principal para executar o script FundFundMe
     function run() external {
@@ -26,24 +20,31 @@ contract FundFundMe is Script {
         //Rodando a função fundFundMe
         fundFundMe(mostRecentlyDeployedContract);
     }
+
+    //Função para financiar o contrato pegando o contrato mais recentemente implantado
+    function fundFundMe(address mostRecentlyDeployedContract) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployedContract)).fund{value: SEND_VALUE}(); //Financiando o contrato com 0.01 ether
+        console.log("Funded FundMe with 0.01 ETH: ", SEND_VALUE);
+        vm.stopBroadcast();
+    }
 }
 
+//Script para sacar o dinheiro do contrato
 contract WithdrawFundMe is Script {
+    //Função principal para executar o script FundFundMe
+    function run() external {
+        //Criando uma variável para armazenar o contrato mais recentemente implantado
+        address mostRecentlyDeployedContract = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+        //Rodando a função fundFundMe
+        withdrawFundMe(mostRecentlyDeployedContract);
+    }
+
     //Função para financiar o contrato pegando o contrato mais recentemente implantado
     function withdrawFundMe(address mostRecentlyDeployed) public {
         vm.startBroadcast();
         FundMe(payable(mostRecentlyDeployed)).withdraw(); //Sacando o dinheiro do contrato
         vm.stopBroadcast();
         console.log("Withdraw FundMe balance!");
-    }
-
-
-    //Função principal para executar o script FundFundMe
-    function run() external {
-        //Criando uma variável para armazenar o contrato mais recentemente implantado
-        address mostRecentlyDeployedContract = DevOpsTools
-            .get_most_recent_deployment("FundMe", block.chainid);
-        //Rodando a função fundFundMe
-        withdrawFundMe(mostRecentlyDeployedContract);
     }
 }
